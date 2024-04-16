cec69303b229f7145a8e99705b0d39af156a1294
Ferozkhan2511 <68701069+Ferozkhan2511@users.noreply.github.com>
2024-04-16 09:36:46 +0530

    Update api.js

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 //const baseUrl = "http://74.249.89.180:5555/api/"
-
+// below code is used to get or replace the value using replace token method in azure devops pipeline
 // const baseUrl = "#{backendurl}#";
   
-  const baseUrl = process.env.baseUrl;
+  const baseUrl = process.env.REACT_APP_API_KEY;
 
 export default {
 
